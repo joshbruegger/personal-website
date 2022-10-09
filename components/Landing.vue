a74c3f807f1b79e0e10e47bfea6cb7696244ad51
Josh Bruegger <josh@bruegger.it>
2022-10-10 00:34:15 +0200

    feat: :sparkles: Added hero image

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="h-screen w-screen flex items-center justify-center bg-black">
-        <div class="">
-            <h1 class="text-9xl text-white text-center">WORK IN PROGRESS</h1>
-            <p class="text-white text-3xl text-center">Come back soon!</p>
+    <section class="h-screen w-screen flex items-center justify-center bg-[#00131C]">
+
+        <div class="absolute left-1/2 -ml-0.5 w-0.5 h-screen bg-gray-600"></div>
+
+        <!-- hero -->
+        <div
+            class="text-white absolute flex justify-center w-screen h-screen  overflow-visible border border-violet-900">
+            <img class="self-end h-[80%] object-cover" src="/hero.webp" alt="Stylized drawing of Josh">
         </div>
 
-    </div>
+
+    </section>
 </template>
 
 <script setup lang="ts">
